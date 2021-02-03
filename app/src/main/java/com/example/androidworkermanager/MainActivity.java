a27409901834f43a83fd4c9b3796f953d4a541f6
Jai Kumar Kherajani <jaikherajani@users.noreply.github.com>
2021-02-03 12:31:35 +0530

    Configure MainActivity and run the background task

--- app/src/main/java/com/example/androidworkermanager/MainActivity.java
+++ app/src/main/java/com/example/androidworkermanager/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.androidworkermanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.NetworkType;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button scheduleJob = findViewById(R.id.trigger_job);
+        scheduleJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Constraints constraints = new Constraints.Builder()
+                        .setRequiresBatteryNotLow(true)
+                        .build();
+
+                PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build();
+
+                WorkManager workManager = WorkManager.getInstance(getApplicationContext());
+                workManager.enqueueUniquePeriodicWork("Counter", ExistingPeriodicWorkPolicy.KEEP,periodicWorkRequest);
+                Toast.makeText(MainActivity.this, "🎉 Scheduled job!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidworkermanager/MyWorker.java
@@ -0,0 +1,30 @@
+package com.example.androidworkermanager;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+
+    private static int count = 0;
+
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        count++;
+        Log.d("MyWorker", "worker called - " + count + " time(s)");
+        return Result.success();
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+}
